d398bd644b0e8fb2e9d70148812703482545f7d6
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-17 01:12:53 -0400

    Inicio de proyecto: Navbar y Sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'maestranza-front';
+  sidebarVisible = false;
+
+  toggleSidebar() {
+    this.sidebarVisible = !this.sidebarVisible;
+  }
+
+  ngAfterViewInit() {
+    this.updateNavbarHeight();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.updateNavbarHeight();
+  }
+
+  updateNavbarHeight() {
+    setTimeout(() => {
+      const navbar = document.querySelector('nav.navbar');
+      if (navbar) {
+        const navbarHeight = navbar.getBoundingClientRect().height;
+        document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+      }
+    }, 0);
+  }
 }
