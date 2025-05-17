944c2fe6f50af5a51df0a327b91427bf36730d65
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-17 01:41:54 -0400

    Se agregar footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, AfterViewInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  standalone: true,
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent, FooterComponent],
 })
 export class AppComponent implements AfterViewInit {
   title = 'maestranza-front';
