b95f9ca2bf30624eb52c2ddd39a9eabfd20ae231
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-05-17 01:57:59 -0400

    se adapta sidebar a footer; se agrega page inventario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent, FooterComponent, CommonModule],
 })
 export class AppComponent implements AfterViewInit {
   title = 'maestranza-front';
@@ -20,20 +21,27 @@ export class AppComponent implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-    this.updateNavbarHeight();
+    this.updateHeights();
   }
 
   @HostListener('window:resize')
   onResize() {
-    this.updateNavbarHeight();
+    this.updateHeights();
   }
 
-  updateNavbarHeight() {
+  updateHeights() {
     setTimeout(() => {
-      const navbar = document.querySelector('nav.navbar');
+      const navbar = document.querySelector('app-navbar');
+      const footer = document.querySelector('app-footer');
+      
       if (navbar) {
         const navbarHeight = navbar.getBoundingClientRect().height;
         document.documentElement.style.setProperty('--navbar-height', `${navbarHeight}px`);
+      }
+      
+      if (footer) {
+        const footerHeight = footer.getBoundingClientRect().height;
+        document.documentElement.style.setProperty('--footer-height', `${footerHeight}px`);
       }
     }, 0);
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { InventarioComponent } from './pages/inventario/inventario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'inventario', component: InventarioComponent },
+]
