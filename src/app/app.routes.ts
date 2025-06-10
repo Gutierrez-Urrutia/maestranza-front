3aa1a099e0bb86569d0c45c4789f672e5b238c42
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2025-06-10 17:21:08 -0400

    se redirigen rutas a login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
-import { InventarioComponent } from './pages/inventario/inventario.component';
 
 export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent)
+    },
     { path: 'inventario', loadComponent: () => import('./pages/inventario/inventario.component').then(m => m.InventarioComponent) },
     { path: 'movimientos', loadComponent: () => import('./pages/movimientos/movimientos.component').then(m => m.MovimientosComponent) },
     { path: 'alertas', loadComponent: () => import('./pages/alertas/alertas.component').then(m => m.AlertasComponent) },
     { path: 'usuarios', loadComponent: () => import('./pages/usuarios/usuarios.component').then(m => m.UsuariosComponent) },
     { path: '', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
+    {
+        path: '**',
+        redirectTo: '/login'
+    }
 ]
